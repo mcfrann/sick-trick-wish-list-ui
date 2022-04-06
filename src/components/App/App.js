13755658e380ef20a10ca1ddb3b27ca4e844cf2c
Francesca McConnell <fmcconnell59@gmail.com>
2022-04-06 09:41:55 -0600

    Setup repo

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,28 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
+import TrickList from './TrickList'
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      tricks: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3001/api/v1/tricks')
+      .then(response => response.json())
+      .then(data => this.setState({
+        tricks: data
+      }))
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Sick Trick Wish List</h1>
+        <TrickList tricks={this.state.tricks}/>
       </div>
     );
   }
