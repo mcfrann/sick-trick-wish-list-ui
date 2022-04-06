5f01843b43eee9aae0ed7aad9e4b1cd6e8cdddad
Francesca McConnell <fmcconnell59@gmail.com>
2022-04-06 11:19:59 -0600

    Pass addTrick down to Form

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -19,11 +19,33 @@ class App extends Component {
       }))
   }
 
+  // postTrick = (newTrick) => {
+  //   fetch('http://localhost:3001/api/v1/tricks', {
+  //     method: 'POST',
+  //     headers: { 'Content-Type': 'application/json' },
+  //     body: JSON.stringify({ stance: this.state.stance,
+  //     name: this.state.name,
+  //     obstacle: this.state.obstacle,
+  //     tutorial: this.state.tutorial
+  //   })
+  //     .then(response => response.json(newTrick))
+  //     .then(data => this.setState({
+  //       tricks: data
+  //     }))
+  //   })
+  // }
+
+  addTrick = (newTrick) => {
+    this.setState({
+      tricks: [...this.state.tricks, newTrick]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Sick Trick Wish List</h1>
-        <Form />
+        <Form addTrick={this.addTrick}/>
         <TrickList tricks={this.state.tricks}/>
       </div>
     );
